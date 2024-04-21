cc6ae74c241df9200bee00a2555549228587a306
Vishnu Nanilal Panicker <vishnunlal@gmail.com>
2024-04-22 00:21:19 +0530

    Fixed issue with highscore JSON conversion from locastorage.

--- src/components/MenuScreen.js
+++ src/components/MenuScreen.js
@@ -8,22 +8,22 @@ export default function MenuScreen() {
     function HandleHighScore() {
         setDisplayHSModal(true)
         let highScoreList = localStorage.getItem('highscore')
-        // console.log(highScoreList)
 
+        //convert to array from JSON and if not present assign to an empty array.
+        highScoreList = highScoreList!=null ? JSON.parse(highScoreList) : []
         highScoreList = highScoreList.sort((x, y) => (y.highscore - x.highscore))
         if (highScoreList.length > 10) {
             highScoreList.pop()
         }
 
         localStorage.setItem('highscore', JSON.stringify(highScoreList))
-
         setHighScores(() => highScoreList)
     }
 
     return (
         <div className='center-cont'>
-            {displayHSModal && <HighScore highscoreList={highScores} closeHSModal={()=>setDisplayHSModal(false)}/>}
-            <Modal openHSModal={()=>HandleHighScore()}/>
+            {displayHSModal && <HighScore highscoreList={highScores} closeHSModal={() => setDisplayHSModal(false)} />}
+            <Modal openHSModal={() => HandleHighScore()} />
         </div>
     )
 }
